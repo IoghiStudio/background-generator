cfa209ce4871bc67bce269eb3c15b97034c04d40
Nicu <ioghigithublab@gmail.com>
2022-06-19 07:23:23 -0700

    installed browserify and moved script.js to bundle.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var _ = require("lodash");
+
+console.log(without);
+
 let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.getElementsByClassName("color2")[0];
